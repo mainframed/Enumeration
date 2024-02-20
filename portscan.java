0d32de319adb385b817e767f6e15775cb0435166
Phil <mainframed767@gmail.com>
2024-02-20 08:28:31 -0800

    Updating portscan.java to support command line timeout and better argument parsing

--- portscan.java
+++ portscan.java
@@ -7,45 +7,65 @@ import java.net.InetAddress;
 // Tool by Owen aka SirCICSAlot
 // 
 
-public class ps
+public class portscan
 {
     public static void main(final String[] args) {
         String host = "";
         int portStart = 0;
         int portEnd = 0;
-        int Debug = 0;
+        int timeout = 1000;
+        boolean debug = false;
         if (args.length < 3 || args[0].toString() == "help") {
-            System.out.println("Usage: host, start port, end port <debug=1>");
+            System.out.println("PortScan by SirCICSalot");
+            System.out.println("Usage: java -cp . portscan host, start port, end port, [-t timeout] [-d debug]");
+            System.out.println("-t timeout is in miliseconds, default is 1000");
+            System.exit(1);
         }
-        else if (args.length == 3) {
-            host = args[0];
+        String host = args[0];
+
+        try {
             portStart = Integer.parseInt(args[1]);
             portEnd = Integer.parseInt(args[2]);
+        } catch (NumberFormatException e) {
+            System.err.println("Invalid port format: " + e.getMessage());
+            System.exit(1);
         }
-        else if (args.length == 4) {
-            host = args[0];
-            portStart = Integer.parseInt(args[1]);
-            portEnd = Integer.parseInt(args[2]);
-            Debug = 1;
+        
+        // Process optional arguments
+        for (int i = 3; i < args.length; i++) {
+            String arg = args[i];
+            if (arg.equals("-t")) {
+                try {
+                    timeout = Integer.parseInt(args[++i]);
+                } catch (NumberFormatException e) {
+                    System.err.println("Invalid timeout format: " + e.getMessage());
+                    System.exit(1);
+                }
+            } else if (arg.equals("-d")) {
+                debug = true;
+            } else {
+                System.err.println("Unknown option: " + arg);
+                System.exit(1);
+            }
         }
-        else {
-            System.exit(0);
-        }
-        if (Debug == 1) {
+
+
+        if (debug) {
             try {
                 final InetAddress ia = InetAddress.getByName(host);
                 System.out.println(ia.getHostAddress());
                 System.out.println(InetAddress.getLocalHost().toString());
+                System.out.println("Timeout: " + timeout);
             }
             catch (Exception ex) {}
         }
         for (int port = portStart; port <= portEnd; ++port) {
-            if (Debug == 1) {
+            if (debug) {
                 System.out.println("Trying Port: " + port);
             }
             try {
                 final Socket socket = new Socket();
-                socket.connect(new InetSocketAddress(host, port), 1000);
+                socket.connect(new InetSocketAddress(host, port), timeout);
                 socket.close();
                 System.out.println("Port " + port + " is open");
             }
